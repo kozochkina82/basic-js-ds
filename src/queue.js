c14261cea232db14e9f61dd09cf48e304c759858
kozochkina82 <kozochkina82@yandex.ru>
2025-01-07 02:30:24 +0500

    feat: add code

--- src/queue.js
+++ src/queue.js
@@ -13,23 +13,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class ListNode {
+  constructor(value) {
+    this.value = value; 
+    this.next = null;   
   }
 }
+class Queue {
+constructor(){
+  this.front = null;
+  this.rear = null;
+}
+
+isEmpty() {
+  return this.front === null;
+}
+
+getUnderlyingList() {
+  return this.front;
+}
+
+getUnderlyingList() {
+    if (this.isEmpty()) {
+      console.log("Queue is empty");
+      return Number.MIN_VALUE;
+  }
+  return this.front.data;
+}
+  }
+  enqueue(value); {
+    const newNode = new ListNode(value); 
+    if (this.isEmpty()) {
+      this.front = this.rear = newNode; 
+    } else {
+      this.rear.next = newNode; 
+      this.rear = newNode;      
+    }
+  }
+
+  dequeue(); {
+    if (this.isEmpty()) {
+      return undefined; 
+    }
+    const removedValue = this.front.value; 
+    this.front = this.front.next;         
+    if (this.front === null) {
+      this.rear = null; 
+    }
+    return removedValue; 
+  }
+
 
 module.exports = {
   Queue
